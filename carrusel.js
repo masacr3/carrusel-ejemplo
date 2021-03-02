9f8b0b50e1b663e35433b5795b05b96b12e38dc3
Leonel R <leorhcp2030@gmail.com>
2021-03-02 00:20:16 -0300

    Add files via upload

--- carrusel.js
+++ carrusel.js
@@ -13,18 +13,26 @@ class Carrusel{
 
     //muestra el nodo actual
     mostrar_actual(){
-        console.log( this.lista[this.actual])
+        let actual = this.lista[this.actual];
+        const nodo = document.querySelector(actual);
+        nodo.classList.add('activar');
     }
 
     //mueve el carrusel a la derecha 
     //si esta en la ultima posicion vuelve a la primera
     mover_derecha(){
+        let actual = this.lista[this.actual];
+        const nodo = document.querySelector(actual);
+        nodo.classList.remove('activar');
         this.actual = (this.actual + 1) == this.cant ? 0 : this.actual + 1;
     }
 
     //mueve el carrusel a la izquierda
     //si esta en la primera posicion lo manda a la ultima  
     mover_izquierda(){
+        let actual = this.lista[this.actual];
+        const nodo = document.querySelector(actual);
+        nodo.classList.remove('activar');
         this.actual = (this.actual - 1) < 0 ? this.cant - 1 : this.actual - 1;
     }
 }
@@ -32,15 +40,21 @@ class Carrusel{
 let carrusel = new Carrusel();
 
 //aca podes agregar la cantidad de datos q quieras
-carrusel.agregar('a');
-carrusel.agregar('b');
-carrusel.agregar('c');
+carrusel.agregar('.a');
+carrusel.agregar('.b');
+carrusel.agregar('.c');
 
 carrusel.mostrar_actual();
-carrusel.mover_derecha();
-carrusel.mover_derecha();
-carrusel.mostrar_actual();
-carrusel.mover_derecha();
-carrusel.mostrar_actual();
-carrusel.mover_izquierda();
-carrusel.mostrar_actual();
+
+const btonizq = document.querySelector('.izq');
+const btonder = document.querySelector('.der');
+
+btonizq.addEventListener('click',()=>{
+    carrusel.mover_izquierda();
+    carrusel.mostrar_actual();
+});
+
+btonder.addEventListener('click',()=>{
+    carrusel.mover_derecha();
+    carrusel.mostrar_actual();
+})
